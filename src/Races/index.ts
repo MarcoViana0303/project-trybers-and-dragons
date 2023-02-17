ab4aa1b49412a70db24a77c84062478d2d4b224b
Marco Viana <marcoviana100@gmail.com>
2023-02-17 17:17:14 -0300

    feat: Criação das subclasses da Race req.2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,8 @@
 import Race from './Race';
+import Halfling from './Halfling';
+import Orc from './Orc';
+import Elf from './Elf';
+import Dwarf from './Dwarf';
 
 export default Race;
+export {Orc, Halfling, Elf, Dwarf};
