3cc2f77a4e629f12e76c2e563794563c7c2e43f0
Marco Viana <marcoviana100@gmail.com>
2023-02-17 19:58:21 -0300

    feat: Criação das subclasses de Archetype req.5

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger.instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
